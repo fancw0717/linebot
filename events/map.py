118b63d321458868cd4cdd8a03ffa7cbf1681249
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-13 01:23:59 +0800

    Add files via upload

---
+++ events/map.py
@@ -0,0 +1,33 @@
+from line_bot_api import *
+
+def goole_map(event):
+    
+    buttons_map = TemplateSendMessage(
+            alt_text= '附近資訊',
+            template=ButtonsTemplate(
+                title='選擇服務',
+                text='請選擇',
+                actions=[
+                    MessageTemplateAction(
+                        label='附近停車場',
+                        text='附近停車場'
+                    ),
+                    MessageTemplateAction(
+                        label='附近加油站',
+                        text='附近加油站'
+                    ),
+                    MessageTemplateAction(
+                        label='附近機車行',
+                        text='附近機車行'
+                    ),
+                    MessageTemplateAction(
+                        label='附近美食',
+                        text='附近美食'
+                    )
+                ]
+            )
+        )
+    line_bot_api.reply_message(
+        event.reply_token, 
+        [buttons_map]
+    )
