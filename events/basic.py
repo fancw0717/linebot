7f5c8d4f285c0bca24a567d372c69dafed5f58d3
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-12 16:59:30 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -54,48 +54,48 @@ Hello! 您好，歡迎您成為 Master Finance的好友!
     line_bot_api.reply_message(
         event.reply_token,
         [text_message, sticker_message, buttons_template])
-# def about_us_event(event):
+def about_us_event(event):
 
-#     emoji = [
-#             {
-#                 "index":0,
-#                 "productId":"5ac2213e040ab15980c9b447",
-#                 "emojiID":"035"
-#             },
-#             {
-#                 "index":11,
-#                 "productId":"5ac2213e040ab15980c9b447",
-#                 "emojiID":"035"
-#             },
-#             {
-#                 "index":23,
-#                 "productId":"5ac1bfd5040ab15980c9b435",
-#                 "emojiID":"091"
+    emoji = [
+            {
+                "index":0,
+                "productId":"5ac2213e040ab15980c9b447",
+                "emojiID":"035"
+            },
+            {
+                "index":11,
+                "productId":"5ac2213e040ab15980c9b447",
+                "emojiID":"035"
+            },
+            {
+                "index":25,
+                "productId":"5ac1bfd5040ab15980c9b435",
+                "emojiID":"091"
 
-#             },
-#             {
-#                 "index":26,
-#                 "productId":"5ac22c9e031a6752fb806d68",
-#                 "emojiID":"042"
+            },
+            {
+                "index":33,
+                "productId":"5ac22c9e031a6752fb806d68",
+                "emojiID":"042"
 
-#             }
-#         ]
-#     text_message = TextSendMessage(text='''$ 歡迎加入 熊安心 $
-# 我是您最安心的小幫手 $ 熊哥
+            }
+        ]
+    text_message = TextSendMessage(text='''$ 歡迎加入 熊安心 $
+我是您最安心的小幫手 $ 熊哥
 
-# $ 機車 相關資訊都可以在這裡找到
-# 點選下方【選單】開始安心上路
+$ 機車 相關資訊都可以在這裡找到
+點選下方【選單】開始安心上路
 
-# ---加入熊安心 騎車更安心---''', emojis=emoji)
+---加入熊安心 騎車更安心---''', emojis=emoji)
     
-#     sticker_message = StickerSendMessage(
-#         package_id='6632',
-#         sticker_id='11825396'
-#     )
+    sticker_message = StickerSendMessage(
+        package_id='6632',
+        sticker_id='11825396'
+    )
     
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         [text_message, sticker_message])
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
 
 # -------------------------------
 #     buttons_template = TemplateSendMessage(
