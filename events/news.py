bca49e61a9c60d2b71fc867a48243c9ab4ab8efd
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-12 18:24:49 +0800

    Add files via upload

--- events/news.py
+++ events/news.py
@@ -14,7 +14,7 @@ def get_latest_news():
     for item in news_items[1:6]:
         news_date = item.find(class_='td').text
         news_title = item.select('.td:nth-child(2) a')[0].text
-        news_link = domain + item.select('.td:nth-child(2) a')[0]['href'].replace('/theme/', '/')
+        news_link = domain + item.select('.td:nth-child(2) a')[0]['href']
         
         news_info = f"日期：{news_date}\n標題：{news_title}\n連結：{news_link}\n"
         latest_news.append(news_info)
