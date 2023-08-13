6c4e354674fc5353111dbb49658a268fa6938da4
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-13 14:47:25 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -29,11 +29,115 @@ def about_us_event(event):
     
     line_bot_api.reply_message(
         event.reply_token,
-        [text_message, sticker_message])
+        [text_message, sticker_message, buttons_template])
 
 # -------------------------------
-#     buttons_template = TemplateSendMessage(
-# {
+    buttons_template = TemplateSendMessage(
+        {
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://i.imgur.com/DfAoQGi.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "我 是 小 幫 手 🐻 熊 哥",
+        "weight": "bold",
+        "size": "xl",
+        "align": "center"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "margin": "md",
+        "contents": [
+          {
+            "type": "text",
+            "text": "👇 請 選 擇 服 務 項 目 👇",
+            "size": "lg",
+            "color": "#999999",
+            "align": "center"
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "uri",
+          "label": "📰  最 新 資 訊",
+          "uri": "https://168.motc.gov.tw/theme/news"
+        },
+        "color": "#5B9A8B"
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "uri",
+          "label": "👍 熊 安 心 粉 絲 團",
+          "uri": "https://linecorp.com"
+        },
+        "color": "#5B9A8B"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "🏍 我 想 看 機 車",
+          "uri": "https://autos.yahoo.com.tw/popular-bikes/"
+        },
+        "style": "primary",
+        "color": "#5B9A8B"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "💬 網 友 最 新 話 題",
+          "uri": "https://www.ptt.cc/bbs/biker/index.html"
+        },
+        "style": "primary",
+        "color": "#5B9A8B"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "postback",
+          "label": "📍 附 近 相 關 支 援",
+          "data": "hello"
+        },
+        "style": "primary",
+        "color": "#5B9A8B"
+      }
+    ],
+    "flex": 0
+  }
+  }
+    )
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     [text_message, sticker_message, buttons_template])
+    
+
+# def show_Button():
+#     flex_message = FlexSendMessage(
+#             alt_text="熊哥幫幫我",
+#             contents={
 #   "type": "bubble",
 #   "hero": {
 #     "type": "image",
@@ -128,110 +232,6 @@ def about_us_event(event):
 #     "flex": 0
 #   }
 # }
+
 #     )
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         [text_message, sticker_message, buttons_template])
-    
-
-def show_Button():
-    flex_message = FlexSendMessage(
-            alt_text="熊哥幫幫我",
-            contents={
-  "type": "bubble",
-  "hero": {
-    "type": "image",
-    "url": "https://i.imgur.com/DfAoQGi.png",
-    "size": "full",
-    "aspectRatio": "20:13",
-    "aspectMode": "cover"
-  },
-  "body": {
-    "type": "box",
-    "layout": "vertical",
-    "contents": [
-      {
-        "type": "text",
-        "text": "我 是 小 幫 手 🐻 熊 哥",
-        "weight": "bold",
-        "size": "xl",
-        "align": "center"
-      },
-      {
-        "type": "box",
-        "layout": "baseline",
-        "margin": "md",
-        "contents": [
-          {
-            "type": "text",
-            "text": "👇 請 選 擇 服 務 項 目 👇",
-            "size": "lg",
-            "color": "#999999",
-            "align": "center"
-          }
-        ]
-      }
-    ]
-  },
-  "footer": {
-    "type": "box",
-    "layout": "vertical",
-    "spacing": "sm",
-    "contents": [
-      {
-        "type": "button",
-        "style": "primary",
-        "action": {
-          "type": "uri",
-          "label": "📰  最 新 資 訊",
-          "uri": "https://168.motc.gov.tw/theme/news"
-        },
-        "color": "#5B9A8B"
-      },
-      {
-        "type": "button",
-        "style": "primary",
-        "action": {
-          "type": "uri",
-          "label": "👍 熊 安 心 粉 絲 團",
-          "uri": "https://linecorp.com"
-        },
-        "color": "#5B9A8B"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "🏍 我 想 看 機 車",
-          "uri": "https://autos.yahoo.com.tw/popular-bikes/"
-        },
-        "style": "primary",
-        "color": "#5B9A8B"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "uri",
-          "label": "💬 網 友 最 新 話 題",
-          "uri": "https://www.ptt.cc/bbs/biker/index.html"
-        },
-        "style": "primary",
-        "color": "#5B9A8B"
-      },
-      {
-        "type": "button",
-        "action": {
-          "type": "postback",
-          "label": "📍 附 近 相 關 支 援",
-          "data": "hello"
-        },
-        "style": "primary",
-        "color": "#5B9A8B"
-      }
-    ],
-    "flex": 0
-  }
-}
-
-    )
-    return flex_message
+#     return flex_message
