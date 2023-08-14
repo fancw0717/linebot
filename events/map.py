cab1febc7380944184268b4d75e6e7687e649ab5
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-14 15:38:28 +0800

    Add files via upload

--- events/map.py
+++ events/map.py
@@ -1,56 +1,37 @@
 from line_bot_api import *
 import requests
 import flask
-def google_map(event):
+
+def google_map():
+    content_text = "附近資訊"
+    text_message = TextSendMessage(
+        text = content_text,
+        quick_reply = QuickReply(
+            items=[
+                QuickReplyButton(
+                        action = MessageAction(
+                            label="附近停車場",
+                            text = "附近停車場",
+                        )
+                ),
+                QuickReplyButton(
+                        action = MessageAction(
+                            label="附近加油站",
+                            text = "附近加油站" 
+                        )
+                ),
+                QuickReplyButton(
+                        action = MessageAction(
+                            label="附近機車行",
+                            text = "附近機車行" 
+                        )
+                )
+            ]
+        ))
     
-    buttons_map = TemplateSendMessage(
-            alt_text= '附近資訊',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                actions=[
-                    MessageTemplateAction(
-                        label='附近停車場',
-                        text='附近停車場'
-                    ),
-                    MessageTemplateAction(
-                        label='附近加油站',
-                        text='附近加油站'
-                    ),
-                    MessageTemplateAction(
-                        label='附近機車行',
-                        text='附近機車行'
-                    ),
-                    MessageTemplateAction(
-                        label='附近美食',
-                        text='附近美食'
-                    )
-                ]
-            )
-        )
-    line_bot_api.reply_message(
-        event.reply_token, 
-        [buttons_map]
-    )
+    return text_message
 
 
-def get_user_location(user_id, api_key):
-    base_url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}"
-    response = requests.post(base_url, json={})
-    data = response.json()
-
-    # if "location" in data:
-    #     return f"{data['location']['lat']},{data['location']['lng']}"
-
-    if "location" in data:
-        location_lat = data['location']['lat']
-        location_lng = data['location']['lng']
-        print(f"使用者 {user_id} 的地點：緯度 {location_lat}, 經度 {location_lng}")
-        return f"{location_lat},{location_lng}"
-    
-    else:
-        print("無法獲取使用者地點。")
-        return None
 
 def search_nearby_parking(location, radius, api_key):
     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
@@ -83,8 +64,6 @@ def callback():
     return 'OK'
 
 
-
-@handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     if event.message.text == '我要分享位置':
         # 讓使用者分享位置
@@ -100,16 +79,63 @@ def handle_text_message(event):
 
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
-    # 使用者已分享位置
+
+    
+    user_id = event.source.user_id
+    api_key = "AIzaSyBuh_ZmBbKBjvtG95pGzaW2-bf77Vc2QoY"
+
+# 使用者已分享位置
     latitude = event.message.latitude
     longitude = event.message.longitude
-    
-    # 進一步處理取得的位置資訊，例如使用 Google Maps API 來查詢地理資訊
-    # ...
 
-    reply_message = f'你的位置是：緯度 {latitude}，經度 {longitude}'
+# 進一步處理取得的位置資訊，例如使用 Google Maps API 來查詢地理資訊
+# ...
+
+
     
+    user_location = f"{latitude},{longitude}"
+    if user_location:
+            radius = 1000
+            nearby_parking = search_nearby_parking(user_location, radius, api_key)
+    
+            if nearby_parking:
+                reply_text = '附近的停車場有：\n'
+                for parking in nearby_parking:
+                    name = parking['name']
+                    address = parking['vicinity']
+                    reply_text += f'名稱: {name}\n地址: {address}\n----------\n'
+            else:
+                reply_text = '附近沒有找到停車場。'
+    else:
+        reply_text = '無法獲取您的地理位置。'
+
+    # selected_option = event.message.text  # 使用者選擇的選項
+
+    # if user_location:
+    #     radius = 1000
+    #     if selected_option == '停車場':
+    #         nearby_places =  search_nearby_parking(user_location, radius, 'parking', api_key)
+    #     elif selected_option == '加油站':
+    #         nearby_places =  search_nearby_parking(user_location, radius, 'gas_station', api_key)
+    #     elif selected_option == '機車行':
+    #         nearby_places =  search_nearby_parking(user_location, radius, 'motorcycle_repair', api_key)
+    #     else:
+    #         reply_text = '請選擇您要查詢的選項：停車場、加油站或機車行。'
+    #         line_bot_api.reply_message(event.reply_token, TextMessage(text=reply_text))
+
+    #     if nearby_places:
+    #         reply_text = f'附近的{selected_option}有：\n'
+    #         for place in nearby_places:
+    #             name = place['name']
+    #             address = place['vicinity']
+    #             reply_text += f'名稱: {name}\n地址: {address}\n----------\n'
+    #     else:
+    #         reply_text = f'附近沒有找到{selected_option}。'
+    # else:
+    #     reply_text = '無法獲取您的地理位置。'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextMessage(text=reply_message)
+        TextMessage(text=reply_text)
     )
+
