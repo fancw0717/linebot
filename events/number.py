d404d760fc5e5d5c114bad1c184e53e3acea10ef
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-15 21:05:48 +0800

    Add files via upload

--- events/number.py
+++ events/number.py
@@ -5,9 +5,9 @@ def get_motor_ptt():
     url = 'https://www.ptt.cc/bbs/biker/index.html'
     re = requests.get(url)
     soup = BeautifulSoup(re.text, 'html.parser')
-    tags = soup.find_all(class_='title')[:5]
+    tags = soup.find_all(class_='title')
     for tag in range(min(5, len(tags))):
-        title = tag.text
-        href = tag.find('a')['href']  # 获取<a>标签中的href属性
+        title = tags[tag].find('a').text
+        href = tags['href']  # 获取<a>标签中的href属性
         content = f"標題: {title}\n連結: https://www.ptt.cc{href}\n"
     return content
