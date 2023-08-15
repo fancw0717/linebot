67abb3bb144c9815d954f26dbc2d31a77a668579
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-15 23:09:24 +0800

    Add files via upload

--- events/number.py
+++ events/number.py
@@ -5,7 +5,7 @@ def get_motor_ptt():
     url = 'https://www.ptt.cc/bbs/biker/index.html'
     re = requests.get(url)
     soup = BeautifulSoup(re.text, 'html.parser')
-    tags = soup.find_all(class_='rent')
+    tags = soup.find_all(class_='r-ent')
     content_list = []  # 創建一個列表來保存標題和連結
 
     for tag in tags[:5]:
