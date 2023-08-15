d9726741b32dd09b2489405c15656a45ae85c8d6
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-15 23:31:30 +0800

    Add files via upload

--- events/number.py
+++ events/number.py
@@ -12,7 +12,7 @@ def get_motor_ptt():
         title = tag.text
         date = tags[tag].find_previous('div', class_='date').text  # 去除前後空白
         href = tag.find('a')['href']  # 获取<a>标签中的href属性
-        content = f"標題: {title}\n連結: https://www.ptt.cc{href}\n {date}\n"
+        content = f"標題: {title}\n連結: https://www.ptt.cc{href}\n{date}\n"
         content_list.append(content)  # 將標題和連結添加到列表中
 
     return content_list
