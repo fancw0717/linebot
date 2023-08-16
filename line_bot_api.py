03fe112a68c3fe217ca4afe48ab72e240ea8893a
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-16 14:14:08 +0800

    Update line_bot_api.py

--- line_bot_api.py
+++ line_bot_api.py
@@ -7,6 +7,6 @@ from linebot.models import *
 app = Flask(__name__)
 
 #Channel access token #在line Developers的providers的Basic settings
-line_bot_api = LineBotApi('aEqzPu8GT8eTBISPBK0ud6aLwnINJ+iZgW8Su9M+T626pCm77+6SE714q2HI3QkbdBQ+iWRs4rsNNNhOJhAP3Q1Q1JE1RUuV+0JVB9pw5riZ5AIJ57uNeJnSwvubHiTQqE9v6Td1j4fBdvQ0FOGcUQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('Fgm1PZljto93lSH0NGfZcisx1YapoloDzRIcm4yUpWRt1luWNX+Ap6A1IcFGGOHpFOKRhMJ5olMurHOH1ylvR3fkOKsxR29KxeURfRj0/vRlZoHdylq+Vt+DQPAyE6vU4pOtZIkh0YTnZniBPYxrKAdB04t89/1O/w1cDnyilFU=')
 #Channel secret  #在line Developers的providers的Messaging API
-handler = WebhookHandler('476740fd390b4f6d726e46b055c207df')
+handler = WebhookHandler('b4d0c49e7ac03e0fdcc331ab66600c0e')
