d19644516cbe9784c06e2f9e1eec44e15bf3d202
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-16 14:29:57 +0800

    Add files via upload

--- events/number.py
+++ events/number.py
@@ -11,7 +11,13 @@ def get_motor_ptt():
     for tag in tags[:5]:
         title = tag.text
         href = tag.find('a')['href']  # 获取<a>标签中的href属性
-        content = f"標題: {title}\n連結: https://www.ptt.cc{href}\n"
-        content_list.append(content)  # 將標題和連結添加到列表中
+        meta_div = tag.find_next('div', class_='meta')
+        date_tag = meta_div.find('div', class_='date')  # 在meta信息中找到日期标签
+        if date_tag:
+            date = date_tag.text.strip()  # 获取日期文本并去除首尾空格
+        else:
+            date = "日期未知"
+            content = f"日期: {date}\n標題: {title}\n連結: https://www.ptt.cc{href}\n"
+            content_list.append(content)  # 將標題和連結添加到列表中
 
     return content_list
