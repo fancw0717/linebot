4f0657a0e110b610020083edd62969d1837942a2
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-16 16:32:59 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -51,8 +51,8 @@ def handle_location_message(event):
             text='請選擇以下的選項',
             actions=[
                 PostbackAction(label='附近停車場', data=f"query={user_location}&type=parking"),
-                #PostbackAction(label='附近充電站', data=f"query={user_location}&type=motorcycle_repair"),
-                PostbackAction(label='附近加油站', data=f"query={user_location}&type=gas_station")
+                PostbackAction(label='附近加油站', data=f"query={user_location}&type=gas_station"),
+                PostbackAction(label='附近美食', data=f"query={user_location}&type=food")
             ]
         )
         template_message = TemplateSendMessage(
@@ -80,8 +80,7 @@ def handle_postback(event):
     if location and place_type:
         radius = 1000
         nearby_places = search_nearby_places(location, radius, place_type, api_key)
-        #places_names_chinese = {'parking': '停車場', 'motorcycle_repair': '充電', 'gas_station': '加油站'}
-        places_names_chinese = {'parking': '停車場', 'gas_station': '加油站'}
+        places_names_chinese = {'parking': '停車場', 'gas_station': '加油站','food':'附近美食'}
         if nearby_places:
             carousel_columns = []
             for place in nearby_places[:10]:  # Limit to 10 due to carousel limitations
