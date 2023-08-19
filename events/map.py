0bdda20624961b42512cafb1f2cd4b94c4d37dac
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-19 18:51:38 +0800

    Add files via upload

--- events/map.py
+++ events/map.py
@@ -37,13 +37,39 @@ def generate_static_map_url(latitude, longitude, api_key):
 
 #——————————————————————————————————————————— map相關參數 —————————————————————————————————————————————————————————
 
-def search_nearby_places(location, radius, place_type=None, api_key="", keyword=None):
+# def search_nearby_places(location, radius, place_type=None, api_key="", keyword=None):
+#     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+#     params = {
+#         "location": location,
+#         "radius": radius,
+#         "key": api_key
+#     }
+    
+#     if place_type:
+#         params["type"] = place_type
+#     if keyword:
+#         params["keyword"] = keyword
+
+#     response = requests.get(base_url, params=params)
+#     data = response.json()
+
+#     if data['status'] == 'OK':
+#         return data['results']
+#     else:
+#         print(f"搜尋附近的地點失敗。狀態:", data['status'])
+#         return []
+
+def search_nearby_places(location, place_type=None, api_key="", keyword=None, rank_by_distance=True):
     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
     params = {
         "location": location,
-        "radius": radius,
         "key": api_key
     }
+    
+    if rank_by_distance:
+        params["rankby"] = "distance"
+    else:
+        params["radius"] = 1000  # or whatever radius you desire if not ranking by distance
     
     if place_type:
         params["type"] = place_type
@@ -58,4 +84,3 @@ def search_nearby_places(location, radius, place_type=None, api_key="", keyword=
     else:
         print(f"搜尋附近的地點失敗。狀態:", data['status'])
         return []
-    
