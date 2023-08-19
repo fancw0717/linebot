90e923b2521d7992de6a835ad27df22e7120d9df
fancw0717 <141933234+fancw0717@users.noreply.github.com>
2023-08-19 19:21:22 +0800

    Add files via upload

--- events/map.py
+++ events/map.py
@@ -37,27 +37,6 @@ def generate_static_map_url(latitude, longitude, api_key):
 
 #——————————————————————————————————————————— map相關參數 —————————————————————————————————————————————————————————
 
-# def search_nearby_places(location, radius, place_type=None, api_key="", keyword=None):
-#     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-#     params = {
-#         "location": location,
-#         "radius": radius,
-#         "key": api_key
-#     }
-    
-#     if place_type:
-#         params["type"] = place_type
-#     if keyword:
-#         params["keyword"] = keyword
-
-#     response = requests.get(base_url, params=params)
-#     data = response.json()
-
-#     if data['status'] == 'OK':
-#         return data['results']
-#     else:
-#         print(f"搜尋附近的地點失敗。狀態:", data['status'])
-#         return []
 
 def search_nearby_places(location, radius=1000, place_type=None, api_key="", keyword=None, rank_by_distance=False):
     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
@@ -84,3 +63,26 @@ def search_nearby_places(location, radius=1000, place_type=None, api_key="", key
     else:
         print(f"搜尋附近的地點失敗。狀態:", data['status'])
         return []
+    
+    
+# def search_nearby_places(location, radius, place_type=None, api_key="", keyword=None):
+#     base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+#     params = {
+#         "location": location,
+#         "radius": radius,
+#         "key": api_key
+#     }
+    
+#     if place_type:
+#         params["type"] = place_type
+#     if keyword:
+#         params["keyword"] = keyword
+
+#     response = requests.get(base_url, params=params)
+#     data = response.json()
+
+#     if data['status'] == 'OK':
+#         return data['results']
+#     else:
+#         print(f"搜尋附近的地點失敗。狀態:", data['status'])
+#         return []
